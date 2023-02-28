9b44d65917a302b8515f30072554c562cd970150
Dharan <opdharanff@gmail.com>
2023-02-28 20:40:06 +0530

    Exercises

---
+++ Neocolabs/Unit-1/Lab_exercise/Practice_exercise/Code1.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Code1{
+     public static void main(String[] args){
+          String name, type;
+          int num;
+          char A;
+          double exp;
+          Scanner scan=new Scanner(System.in);
+
+          name=scan.nextLine();
+          type=scan.nextLine();
+          num=scan.nextInt();
+          A=scan.next().charAt(0);
+          exp=scan.nextDouble();
+
+          System.out.println("Event Name : "+name);
+          System.out.println("Event Type : "+type);
+          System.out.println("Expected Count : "+num);
+          System.out.println("Paid Entry : 11 "+A);
+          System.out.println("Projected Expense : "+exp+"L");
+
+          scan.close();
+     }
+}
