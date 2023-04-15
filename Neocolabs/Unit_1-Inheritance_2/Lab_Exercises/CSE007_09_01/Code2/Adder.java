3c884bb1ce9030efb51b34d54405160908e123d6
Dharan <opdharanff@gmail.com>
2023-04-15 22:09:30 +0530

    JAVA Neocolabs Unit_1-Inheritance_2 Lab_Exercises CSE007_09_01 Code1 Code2

---
+++ Neocolabs/Unit_1-Inheritance_2/Lab_Exercises/CSE007_09_01/Code2/Adder.java
@@ -0,0 +1,7 @@
+//Write the following code - A class named Arithmetic - A class named Adder
+//Adder.java
+class Adder extends Arithmetic {
+    public int callAdd(int a, int b) {
+        return add(a, b);
+    }
+}
---
+++ Neocolabs/Unit_1-Inheritance_2/Lab_Exercises/CSE007_09_01/Code2/Arithmetic.java
@@ -0,0 +1,8 @@
+//Write the following code - A class named Arithmetic - A class named Adder
+//Arithmetic.java
+class Arithmetic {
+    public int add(int a, int b) {
+        int sum = a + b;
+        return sum;
+    }
+}
