3a8c505b34a8fa8a6bc9c0c88b8a8b735df074c3
Dharan <opdharanff@gmail.com>
2023-04-15 21:59:19 +0530

    JAVA Neocolabs

---
+++ Neocolabs/Unit_1-Inheritance_2/Lab_Exercises/CSE007_09/Code1.java
@@ -0,0 +1,27 @@
+//Using inhertiance, one class can acquire the properties of other - Consider the follwing animal classe
+class Animal {
+    void walk() {
+        System.out.println("I am walking");
+    }
+}
+
+class Bird extends Animal {
+    void fly() {
+        System.out.println("I am flying");
+    }
+}
+
+class Human extends Bird {
+    void sing() {
+        System.out.println("I am singing");
+    }
+}
+
+class Code1 {
+    public static void main(String[] args) {
+        Human obj = new Human();
+        obj.walk();
+        obj.fly();
+        obj.sing();
+    }
+}
