21d41eb0841e6fb795b922774bf74628fcdd5d96
Dharan <opdharanff@gmail.com>
2023-04-18 21:18:10 +0530

    JAVA Neocolabs Unit_1-Inheritance_2 Lab_Exercises CSE007_09_01 Code1 Main.java JAVA Neocolabs Unit_1-Inheritance_2 Lab_Exercises CSE007_09_01 Code2 Main.java

--- Neocolabs/Unit_1-Inheritance_2/Lab_Exercises/CSE007_09_01/Code1/Main.java
+++ Neocolabs/Unit_1-Inheritance_2/Lab_Exercises/CSE007_09_01/Code1/Main.java
@@ -1,3 +1,33 @@
+// Write the following code in your editor below:
+
+//     A class named Arithmetic with a method named add that takes  
+//     integers as parameters and returns an integer denoting their sum.
+//     A class named Adder that inherits from a superclass named 
+//     Arithmetic. and a member function callAdd which calls base 
+//     class (Arithmetic) add function.
+
+// Your classes should not be be public (except main class).
+
+// the main class is given as hint
+// Input format
+
+// num1 num2
+
+// num3 num4
+// Output format
+
+// please see the sample output
+// Sample testcases
+// Input 1
+
+// 2 4
+// 9 3
+
+// Output 1
+
+// My superclass is: Arithmetic
+// 6 12
+
 //Design a class named Person
 //Main.java
 public class Main {
--- Neocolabs/Unit_1-Inheritance_2/Lab_Exercises/CSE007_09_01/Code2/Main.java
+++ Neocolabs/Unit_1-Inheritance_2/Lab_Exercises/CSE007_09_01/Code2/Main.java
@@ -1,3 +1,63 @@
+// Design a class named Person and its two subclasses named 
+// Student and Employee.
+
+// Make Faculty and Staff subclasses of Employee.
+
+// A person has a name, address, phone number, and email address.
+
+// A student has a class status (string: freshman, sophomore, 
+// junior, or senior).
+
+// An employee has an office and salary.
+
+// Override the toString method in each class to display the class 
+// name and the person’s info / students info / employee info.
+
+
+// Create a Person (object name: person ) name: "John" address: 
+// "12 Bell street",   phone number: "3473339999", and email address: 
+// "john12@aol.com"
+
+// Create a Student (object name: student ) name: "Mary", address: 
+// "100 Town Ave", phone number: "5149993333", email address: 
+// "mary100@aol.com", and status "freshman"
+
+// Create a Employee (object name: employee ) name: "Mike", address: 
+// "34 West street", phone number: "6189999999", email address:	
+// "mike80@aol.com", office: 910, and salary: 60000
+
+
+// Input format
+
+// <no input required>'
+
+// create the objects as specified
+// Output format
+
+// Please see the sample output
+// Sample testcases
+// Input 1
+
+// Output 1
+
+
+// Name: John
+// Address: 12 Bell street
+// Phone number: 3473339999
+// Email address: john12@aol.com
+
+// Name: Mary
+// Address: 100 Town Ave
+// Phone number: 5149993333
+// Email address: mary100@aol.com
+// Status: freshman
+
+// Name: Mike
+// Address: 34 West street
+// Phone number: 6189999999
+// Email address: mike80@aol.com
+// Office: 910
+// Salary: 60000.00
 
 //Write the following code - A class named Arithmetic - A class named Adder
 //Main.java
