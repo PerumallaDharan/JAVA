69dc2a228bc6c7683cf86b301f45675e735c07d1
Dharan <opdharanff@gmail.com>
2023-05-13 16:00:58 +0530

    Lab_Exercises

---
+++ Neocolabs/Unit_3-Streams/Lab_Exercises/Code3.java
@@ -0,0 +1,21 @@
+
+// Gokul is given a list
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
+class Code3 {
+    public static void main(String args[]) {
+        List<Integer> arList = new ArrayList<Integer>();
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        for (int i = 0; i < num; i++) {
+            arList.add(scan.nextInt());
+        }
+        arList.stream()
+                .filter(n -> n % 2 == 0)
+                .forEach(System.out::println);
+
+        scan.close();
+    }
+} 
