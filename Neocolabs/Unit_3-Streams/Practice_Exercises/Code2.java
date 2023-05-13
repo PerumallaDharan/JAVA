5ba6bfde36d27f9a47aae242b4612a77bf37149c
Dharan <opdharanff@gmail.com>
2023-05-13 16:09:46 +0530

    Practice_Exercises

---
+++ Neocolabs/Unit_3-Streams/Practice_Exercises/Code2.java
@@ -0,0 +1,21 @@
+
+// Given a list of integers
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
+class Code2 {
+    public static void main(String[] args) {
+        List<Integer> arList = new ArrayList<Integer>();
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        for (int i = 0; i < num; i++) {
+            arList.add(scan.nextInt());
+        }
+        long count = arList.stream()
+                .count();
+        System.out.println(count);
+
+        scan.close();
+    }
+}
---
+++ Neocolabs/Unit_3-Streams/Practice_Exercises/Code3.java
@@ -0,0 +1,22 @@
+
+// Helen is having
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
+class Code3 {
+    public static void main(String[] args) {
+        List<Integer> arList = new ArrayList<Integer>();
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        for (int i = 0; i < num; i++) {
+            arList.add(scan.nextInt());
+        }
+        int max = arList.stream()
+                .max(Integer::compare)
+                .get();
+        System.out.println(max);
+
+        scan.close();
+    }
+}
